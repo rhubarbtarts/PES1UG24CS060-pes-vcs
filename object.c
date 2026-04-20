d0e1a4e68a40ce52ebc4b8f5be0851439c6ea33b
rhubarbtarts <ananyauppili@gmail.com>
2026-04-20 06:39:50 +0000

    added header creation in object_write

--- object.c
+++ object.c
@@ -95,6 +95,8 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
+char header[64];
+int header_len = snprintf(header, sizeof(header), "%s %zu", type, size);
     (void)type; (void)data; (void)len; (void)id_out;
     return -1;
 }
